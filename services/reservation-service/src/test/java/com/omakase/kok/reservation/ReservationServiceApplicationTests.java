0e63c70dab0edccb8529f7990292530fe99b695c
riiach <riiachoii@gmail.com>
2026-06-15 21:20:44 +0900

    feat: Reservation Service 초기 세팅 추가

---
+++ services/reservation-service/src/test/java/com/omakase/kok/reservation/ReservationServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.omakase.kok.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

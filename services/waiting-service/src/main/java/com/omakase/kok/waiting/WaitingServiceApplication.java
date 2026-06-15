fa5d1d9b66c26811c83daac68ca835d48822d044
riiach <riiachoii@gmail.com>
2026-06-15 21:30:22 +0900

    feat: Waiting Service 초기 세팅 추가

---
+++ services/waiting-service/src/main/java/com/omakase/kok/waiting/WaitingServiceApplication.java
@@ -0,0 +1,15 @@
+package com.omakase.kok.waiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class WaitingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitingServiceApplication.class, args);
+	}
+
+}

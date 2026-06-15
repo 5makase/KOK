aee2a6e3442f766578b48cef67965c2391cd854f
riiach <riiachoii@gmail.com>
2026-06-15 14:17:43 +0900

    fix: 테스트 빌드를 위해 서비스에 h2 추가

--- services/user-service/src/main/java/com/omakase/kok/user/UserServiceApplication.java
+++ services/user-service/src/main/java/com/omakase/kok/user/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.omakase.kok.user_service;
+package com.omakase.kok.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- services/user-service/src/test/java/com/omakase/kok/user/UserServiceApplicationTests.java
+++ services/user-service/src/test/java/com/omakase/kok/user/UserServiceApplicationTests.java
@@ -1,13 +1,14 @@
-package com.omakase.kok.user_service;
+package com.omakase.kok.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }

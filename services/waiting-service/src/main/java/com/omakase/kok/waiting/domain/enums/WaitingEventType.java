0fccb0605c57cf442afc4c379d2d4969cf8c1d79
hyeeuncho <heeuncho@naver.com>
2026-06-17 12:21:12 +0900

    feat: Entity 구현

---
+++ services/waiting-service/src/main/java/com/omakase/kok/waiting/domain/enums/WaitingEventType.java
@@ -0,0 +1,9 @@
+package com.omakase.kok.waiting.domain.enums;
+
+public enum WaitingEventType {
+    WAITING_REGISTERED,
+    WAITING_CALLED,
+    WAITING_ENTERED,
+    WAITING_CANCELLED,
+    WAITING_NO_SHOW
+}
---
+++ services/waiting-service/src/main/java/com/omakase/kok/waiting/domain/enums/WaitingStatus.java
@@ -0,0 +1,9 @@
+package com.omakase.kok.waiting.domain.enums;
+
+public enum WaitingStatus {
+    WAITING,
+    CALLED,
+    ENTERED,
+    CANCELLED,
+    NO_SHOW
+}

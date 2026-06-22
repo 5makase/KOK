6bee7bdb0ade1d1667db1da78c2158b4b56d7895
LeeSG-0114 <leesin0114@naver.com>
2026-06-22 10:52:46 +0900

    feat: 응답 dto 구현

---
+++ services/notification-service/src/main/java/com/omakase/kok/notification/dto/response/NotificationResponse.java
@@ -0,0 +1,35 @@
+package com.omakase.kok.notification.dto.response;
+
+import com.omakase.kok.notification.entity.Notification;
+import com.omakase.kok.notification.enums.NotificationType;
+import com.omakase.kok.notification.enums.ReferenceType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Builder
+public class NotificationResponse {
+
+    private UUID notificationId;
+    private UUID referenceId;
+    private ReferenceType referenceType;
+    private NotificationType notificationType;
+    private String message;
+    private boolean isRead;
+    private LocalDateTime createdAt;
+
+    public static NotificationResponse from(Notification notification) {
+        return NotificationResponse.builder()
+                .notificationId(notification.getNotificationId())
+                .referenceId(notification.getReferenceId())
+                .referenceType(notification.getReferenceType())
+                .notificationType(notification.getNotificationType())
+                .message(notification.getMessage())
+                .isRead(notification.isRead())
+                .createdAt(notification.getCreatedAt())
+                .build();
+    }
+}

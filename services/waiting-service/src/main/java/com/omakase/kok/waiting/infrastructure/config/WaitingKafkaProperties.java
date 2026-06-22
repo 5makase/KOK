69f91256742d14930580ceba14ff73f8217e56c1
hyeeuncho <heeuncho@naver.com>
2026-06-22 16:11:01 +0900

    fix(waiting): 카프카 설정값 검증 추가

--- services/waiting-service/src/main/java/com/omakase/kok/waiting/infrastructure/config/WaitingKafkaProperties.java
+++ services/waiting-service/src/main/java/com/omakase/kok/waiting/infrastructure/config/WaitingKafkaProperties.java
@@ -1,11 +1,20 @@
 package com.omakase.kok.waiting.infrastructure.config;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "waiting.kafka.topic.waiting-events")
 public record WaitingKafkaProperties(
+        @NotBlank
         String name,
+
+        @Min(1)
         int partitions,
+
+        @Min(1)
         short replicas
 ) {
 }
